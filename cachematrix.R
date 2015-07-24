5261699b8618d0073da7ca210b598c09b9038192
Dan Davis <ddavis829@gmail.com>
2015-07-24 17:49:26 -0400

    Commiting ProgrammingHomework2 work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are to facilitate the caching of an inverse of a matrix
+## to increase calculation performance of repeated tasks
 
-## Write a short comment describing this function
 
+## makeCacheMatrix creates a container to store a solved matrix to be used in caching
 makeCacheMatrix <- function(x = matrix()) {
-
+    # container to store the solved matrix
+    i <- NULL
+    
+    # function to set the unsolved matrix to x and clear cache
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    
+    # function to retrieve the original matrix
+    get <- function() x
+    
+    # function to set the cache to the solved matrix
+    setinverse <- function(inv) i <<- inv
+    
+    # function to retrieve the cached value(will return NULL if not already set)
+    getinverse <- function() i
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve encompasses the cached object
+## it will return the cached solved matrix, if available
+## if the cached solved matrix is not available, it will solve the original matrix,
+## store it in cache, and return the solved matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # retrieve the cached value of the solved matrix
+    i <- x$getinverse()
+    
+    # check if the cached value is valid by a NULL check
+    if (!is.null(i)) {
+        # value was already cached, return the value
+        message("Getting cached data")
+        return (i)
+    }
+    
+    # value was not cached, get the original matrix and store the solved matrix in i
+    i = solve(x$get(), ...)
+    
+    # set the cache to the value of i for subsequent retrieval
+    x$setinverse(i)
+    
+    # return the solved matrix
+    i
 }
